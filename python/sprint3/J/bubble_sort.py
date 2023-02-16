f62cac68bfd16bbe328bf793ee2430712f98bcce
Andrey <Andrey>
2023-02-16 23:13:20 +0100

    Sprint3 Task J Bubble sort

--- python/sprint3/J/bubble_sort.py
+++ python/sprint3/J/bubble_sort.py
@@ -2,13 +2,25 @@ import sys
 
 
 def read_input():
-    n = input()
-    arr = sys.stdin.readline().strip().split()
+    n = int(input())
+    arr = list(map(int, sys.stdin.readline().strip().split()))
     return n, arr
 
 def bubble_sort(n, arr):
-    pass
+    for j in range(n-1,0,-1):
+        permutations = 0
+        for i in range(j):
+            if arr[i] > arr[i+1]:
+                arr[i], arr[i+1] = arr[i+1], arr[i]
+                permutations += 1
+        if permutations:
+            print(*arr)
+        else:
+            if j == n-1:
+                print(*arr)
+            return
+
 
 if __name__ == '__main__':
     n, arr = read_input()
-    print(bubble_sort(n, arr))
+    bubble_sort(n, arr)
